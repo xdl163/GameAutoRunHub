f86c68279d5ab4df6fb3664510145bbbc0543303
xdl <1056102915@qq.com>
2025-12-21 00:21:05 +0800

    添加android打包配置

---
+++ static/config.js
@@ -0,0 +1,12 @@
+// js/config.js
+export function isCapacitorNative() {
+    return !!(
+        window.Capacitor &&
+        window.Capacitor.isNativePlatform &&
+        window.Capacitor.isNativePlatform()
+    );
+}
+
+export const API_BASE = isCapacitorNative()
+    ? "http://83.229.125.195:8055/"   // App 环境
+    : "";                          // 浏览器同源
